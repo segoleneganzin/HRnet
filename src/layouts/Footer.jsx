6bde7627c27dc97adfd71213debc045e02b111ad
Segolene GANZIN <segolene.ganzin@gmail.com>
2024-08-27 11:07:30 +0200

    add footer content

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,5 +1,10 @@
 const Footer = () => {
-  return <footer className='footer'>Footer</footer>;
+  return (
+    <footer className='footer'>
+      <p>© 2024 Wealth Health. All rights reserved.</p>
+      <p>Contact us: info@wealth-health.com</p>
+    </footer>
+  );
 };
 
 export default Footer;
